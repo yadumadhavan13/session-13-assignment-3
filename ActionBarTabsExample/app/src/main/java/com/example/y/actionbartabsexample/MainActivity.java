e0a8cd33fba78276665c0e662d4f0156d0c4fa20
yadumadhavan13 <yadumadhavan13@gmail.com>
2016-10-17 23:24:16 +0530

    Implement an action bar with tabs
    ActionBar with 2 tabs - audio and video.both tab should have a text
    view.

---
+++ ActionBarTabsExample/app/src/main/java/com/example/y/actionbartabsexample/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.y.actionbartabsexample;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.TabListener;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements TabListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setLogo(R.drawable.ic);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setNavigationMode(actionBar.NAVIGATION_MODE_TABS);
+        actionBar.addTab(actionBar.newTab().setText("AUDIO").setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("VIDEO").setTabListener(this));
+
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        int tabselected = tab.getPosition();
+        switch(tabselected){
+            case 0:{
+                setContentView(R.layout.tab1);
+                return;
+
+            }
+            case 1:{
+                setContentView(R.layout.tab2);
+                return;
+            }
+
+        }
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+}
